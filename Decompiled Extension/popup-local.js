32fe117f2526b9564d7dcc541bb0f341a0be2583
Matt Coady <matt@unbounce.com>
2015-05-22 11:55:25 -0700

    Setup for 3 environments

--- Decompiled Extension/popup-local.js
+++ Decompiled Extension/popup-local.js
@@ -1,31 +1,35 @@
 window.onload=function(){
 
-  var curURL;
+  var curURL, imgURL;
 
-  if(window.location.href.match(/localhost/).length>=1){
+  if(window.location.href.match(/localhost/)){
     curURL = 'local';
-  }else if(window.location.href.match(/app-test\.unbounce/).length>=1){
+  }else if(window.location.href.match(/app-test\.unbounce/)){
     curURL = 'appTest';
-  }else if(window.location.href.match(/app-test-builder\.unbounce/).length>=1){
+  }else if(window.location.href.match(/app-test-builder\.unbounce/)){
     curURL = 'appTestBuilder'
   }
 
   function icoLoad(){
-    if(headers.includes){
-      document.head = document.head || document.getElementsByTagName('head')[0];
+    document.head = document.head || document.getElementsByTagName('head')[0];
 
-      function changeFavicon(src) {
-        var link = document.createElement('link'),
-          oldLink = document.getElementById('dynamic-favicon');
-        link.id = 'dynamic-favicon';
-        link.rel = 'shortcut icon';
-        link.href = src;
-        if (oldLink) document.head.removeChild(oldLink);
-        document.head.appendChild(link);
-      }
-      var imgURL = chrome.extension.getURL("dev.ico");
-      changeFavicon(imgURL);
+    function changeFavicon(src) {
+      var link = document.createElement('link'),
+        oldLink = document.getElementById('dynamic-favicon');
+      link.id = 'dynamic-favicon';
+      link.rel = 'shortcut icon';
+      link.href = src;
+      if (oldLink) document.head.removeChild(oldLink);
+      document.head.appendChild(link);
     }
+
+    switch(curURL){
+      case 'local': imgURL = chrome.extension.getURL("icon-green-icon.ico"); break;
+      case 'appTest': imgURL = chrome.extension.getURL("icon-orange-icon.ico"); break;
+      case 'appTestBuilder': imgURL = chrome.extension.getURL("icon-purple-icon.ico"); break;
+    }
+
+    changeFavicon(imgURL);
   }
 
   icoLoad();
